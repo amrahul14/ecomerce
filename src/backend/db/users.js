c29030a04328263697ba61a3878abd4b09f56f75
Rahul <rahul15kumar9708@gmail.com>
2023-08-12 11:44:06 +0530

    first commit

---
+++ src/backend/db/users.js
@@ -0,0 +1,32 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * Every user will have cart (Quantity of all Products in Cart is set to 1 by default), wishList by default
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Rahul",
+    lastName: "Kumar",
+    email: "gautamshekhar078@gmail.com",
+    password: "Gautam#123",
+    address: [
+      {
+        _id: uuid(),
+        name: "Rahul kumar",
+        zipCode: "805128",
+        street: "Road no 1 , Ashok nagar , kankarbagh",
+        city: "Patna",
+        district: "Patna",
+        state: "Bihar",
+        country: "India",
+        phoneNumber: "7779843621"
+      }
+    ],
+    createdAt: formatDate(),
+    updatedAt: formatDate()
+  }
+];
